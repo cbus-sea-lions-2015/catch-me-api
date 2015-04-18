2e9d38d800547607d8bf2f20c96cde13d7c98408
Jose Roman <jferoman@gmail.com>
2015-04-17 23:01:24 -0400

    authenticate api method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
 
-	
+  def authenticate
+    user = User.find_by(username: [:username])
+    if user && user.authenticate([:password])
+      render json: user.id
+    else
+       render json: false
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
     resource :courses_points, except: [:new, :edit]
   end
   resources :users, except: [:new, :edit]
-
+  
+  post 'users/authenticate'=> 'users#authenticate'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
