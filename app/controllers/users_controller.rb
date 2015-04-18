0ef65513750fee26bbd08f1d6e2be161dfa8e16c
Jose Roman <jferoman@gmail.com>
2015-04-18 13:52:09 -0400

    reset users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    # user = User.new(user_params)
-    logger.info params
-    user = User.new(username: params[:username],name: params[:name],handle: params[:handle],password: params[:password])
+    user = User.new(user_params)
     user.save
   end
 
-   # private
+   private
 
-   # def  user_params
-   #   params.require(:register_data).permit(:username,:name,:handle,:password)
-   # end
+   def  user_params
+     params.require(:register_data).permit(:username,:name,:handle,:password)
+   end
 end
