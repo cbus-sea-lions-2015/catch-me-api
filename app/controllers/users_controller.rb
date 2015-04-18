fa20e65ee6aa6060df7488bec7f2d6a8a8367bea
Jose Roman <jferoman@gmail.com>
2015-04-18 13:50:54 -0400

    changed users controller for json response input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    # user = User.new(user_params)
+    logger.info params
+    user = User.new(username: params[:username],name: params[:name],handle: params[:handle],password: params[:password])
     user.save
   end
 
-   private
+   # private
 
-   def  user_params
-     params.require(:register_data).permit(:username,:name,:handle,:password)
-   end
+   # def  user_params
+   #   params.require(:register_data).permit(:username,:name,:handle,:password)
+   # end
 end
