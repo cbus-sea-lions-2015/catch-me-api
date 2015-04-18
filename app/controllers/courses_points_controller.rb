4bea107c1e5a6120107a90d2358eabbb755796a0
Jose Roman <jferoman@gmail.com>
2015-04-18 12:13:01 -0400

    user create method added

--- app/controllers/courses_points_controller.rb
+++ app/controllers/courses_points_controller.rb
@@ -4,7 +4,16 @@ class CoursesPointsController < ApplicationController
       @course_points = Course.find_by(id: params[:course_id]).courses_points
       render json: @course_points
 	end
+
+	def create
+	  course = Course.find_by(id: params[:course_id])
+      course.courses_points.create(course_point_params)
+	end
     
-    
+    private
+
+    def course_point_params
+      params.require(:course_point).permit(:longitude,:latitude,:altitude)
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def create
+    user = User.new(user_params)
+    user.save
+  end
 
+   private
+
+   def  user_params
+     params.require(:register_data).permit(:username.:name,:handle,:password)
+   end
 end
